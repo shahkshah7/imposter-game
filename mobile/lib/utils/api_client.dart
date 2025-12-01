af446155adf0a2965fa8f3b31288497cc8c22586
shahkshah7 <shahkshah7@gmail.com>
2025-12-01 23:44:18 +0000

    Update ApiClient with timeouts and full post signature

--- mobile/lib/utils/api_client.dart
+++ mobile/lib/utils/api_client.dart
@@ -1,19 +1,35 @@
-import 'dart:io' show Platform;
-
+import 'dart:io';
 import 'package:dio/dio.dart';
 
 class ApiClient {
-  static final String _baseUrl =
-      Platform.isAndroid ? "http://10.0.2.2:8000/api" : "http://127.0.0.1:8000/api";
-
   static final Dio dio = Dio(
     BaseOptions(
-      baseUrl: _baseUrl,
+      baseUrl: Platform.isAndroid
+          ? "http://10.0.2.2:8000/api"
+          : "http://127.0.0.1:8000/api",
       connectTimeout: Duration(seconds: 5),
       receiveTimeout: Duration(seconds: 5),
-      headers: {
-        "Accept": "application/json",
-      },
+      headers: {"Accept": "application/json"},
     ),
   );
+
+  static Future<Response<dynamic>> post(
+    String path, {
+    dynamic data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+    CancelToken? cancelToken,
+    ProgressCallback? onSendProgress,
+    ProgressCallback? onReceiveProgress,
+  }) {
+    return dio.post(
+      path,
+      data: data,
+      queryParameters: queryParameters,
+      options: options,
+      cancelToken: cancelToken,
+      onSendProgress: onSendProgress,
+      onReceiveProgress: onReceiveProgress,
+    );
+  }
 }
